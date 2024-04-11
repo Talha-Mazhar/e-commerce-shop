37cf7a5be4863b03884fd6256306bf4e4674f916
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-11 19:08:46 +0500

    react router with latest implementaion with eror handling added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen.jsx'
+import NotFound from './screens/NotFound.jsx'
+import ErrorPage from './screens/ErrorPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Routes>
+                <Route path='/' element={<App />} errorElement={<ErrorPage />}>
+                    <Route
+                        index
+                        element={<HomeScreen />}
+                        errorElement={<ErrorPage />}
+                    />
+                </Route>
+                <Route
+                    path='*'
+                    element={<NotFound />}
+                    errorElement={<ErrorPage />}
+                />
+            </Routes>
+        </Router>
     </React.StrictMode>
 )
