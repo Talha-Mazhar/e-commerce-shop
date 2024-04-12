e1d50e579b0f9687e45af8fe3748f19e26c63e9f
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-12 16:29:42 +0500

    seeding script added for products and users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
             unique: true,
         },
         password: {
-            type: true,
+            type: String,
             required: true,
         },
         isAdmin: {
