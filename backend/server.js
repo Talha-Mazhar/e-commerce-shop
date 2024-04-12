1d295dc7c08cf04d5782915f9bd2ccbb17732d83
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-12 21:12:10 +0500

    custom error handler adde

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 //env config
 dotenv.config()
@@ -20,6 +21,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
