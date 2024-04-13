ad27414b28d33f3b6de610e796e0320f99dd5e86
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-13 17:40:35 +0500

    product apiSlice is adde

--- frontend/src/store/slices/product/productApiSlice.js
+++ frontend/src/store/slices/product/productApiSlice.js
@@ -1,6 +1,15 @@
+import { query } from 'express'
 import { PRODUCTS_URL } from '../../constants'
 import { apiSlice } from '../apiSlice'
 
 export const productApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({}),
+    endpoints: builder => ({
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCTS_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+    }),
 })
+export const { useGetProductsQuery } = productApiSlice
