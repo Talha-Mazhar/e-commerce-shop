2b36588267fce18e0750903f61ff1f46edb79e11
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-17 13:48:35 +0500

    logged out added->cookie clear

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,13 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/logout
 //@access Private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.json('logout user')
+    //clear the cookie with jwt etc
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    })
+
+    res.status(200).json({ message: 'Logged out successfully' })
 })
 
 //@desc Logout user
