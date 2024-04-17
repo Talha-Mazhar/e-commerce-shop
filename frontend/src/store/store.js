83577b1cf00395423450a3d17d56e0164836d441
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-04-17 14:45:44 +0500

    user action added

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice'
 import cartSliceReducer from './slices/cart/cartSlice'
+import authSliceReducer from './slices/users/authSlice'
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReducer,
+        auth: authSliceReducer,
     },
     middleware: getDefualtMiddleware =>
         getDefualtMiddleware().concat(apiSlice.middleware),
