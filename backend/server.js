2512b34255c98f8fa4827aacf44863cf4a0c2320
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-10-22 22:26:19 +0500

    deployed seuccessfully erros resolved backend

--- backend/server.js
+++ backend/server.js
@@ -47,18 +47,18 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  // Routes
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+//   );
+// } else {
+//   // Routes
+//   app.get("/", (req, res) => {
+//     res.send("API is running...");
+//   });
+// }
 
 // Error handling middleware
 app.use(notFound);
